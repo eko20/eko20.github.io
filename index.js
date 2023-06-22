e8c6fbc641f8677546859dfbe20e6af5b61c18fe
eko20 <109029696+eko20@users.noreply.github.com>
2023-06-22 17:48:53 +0300

    Add files via upload
    new version

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
   const rangeInput = $("#rangeInput");
   const rangeValue = $("#rangeValue");
-  const generate = $(".btnn");
+  const copy = $(".copy");
+  const refresh = $(".refresh");
   const checkBoxPun = $("#flexCheckChecked1");
   const checkBoxMix = $("#flexCheckChecked2");
   const passwordContainer = $(".passwords");
-
-  rangeInput.on("input", function () {
-    const value = $(this).val();
-    rangeValue.text(value);
-  });
 
   function generatePassword() {
     const length = rangeInput.val();
@@ -25,29 +21,86 @@ $(document).ready(function () {
     let password = " ";
     let charathers = upperCase + lowerCase + number;
     if (includePun) {
-        charathers += punctation;
+      charathers += punctation;
     }
     if (includeMix) {
-        charathers += mixedCase;
+      charathers += mixedCase;
     }
 
-    const array = new Uint32Array(length)
-    window.crypto.getRandomValues(array)
+    const array = new Uint32Array(length);
+    window.crypto.getRandomValues(array);
 
     for (let i = 0; i < length; i++) {
-        password += charathers[array[i] % charathers.length];
-        
+      password += charathers[array[i] % charathers.length];
     }
-    
-    return password;
 
+    return password;
   }
 
-  generate.click(function() {
+  function passwordWrite() {
     const password = generatePassword();
     passwordContainer.text(password);
+    passwordDifficulty();
+  }
 
+  function passwordDifficulty() {
+    let passDif = "very strong";
+    $(".strong").css("color", "red");
+    const length = parseInt(rangeInput.val(), 10);
+
+    if (length >= 15 && length < 20) {
+      passDif = "strong";
+      $(".strong").css("color", "red");
+    } else if (length >= 10 && length < 15) {
+      passDif = "good";
+      $(".strong").css("color", "blue");
+    } else if (length >= 5 && length < 10) {
+      passDif = "weak";
+      $(".strong").css("color", "green");
+    }
+    $(".strong").text(passDif);
+  }
+
+  rangeInput.on("input", function () {
+    const value = $(this).val();
+    rangeValue.text(value);
+    passwordWrite();
   });
-  
 
+  checkBoxMix.click(function () {
+    passwordWrite();
+  });
+
+  checkBoxPun.click(function () {
+    passwordWrite();
+  });
+
+  passwordWrite();
+
+  copy.on("click", function () {
+    let copyText = $(".passwords");
+    let curentPassword = copyText.text();
+
+    navigator.clipboard
+      .writeText(curentPassword)
+      .then(function () {
+        console.log("Text copied to clipboard");
+      })
+      .catch(function (error) {
+        console.error("Unable to copy text to clipboard:", error);
+      });
+  });
+
+  refresh.click(function () {
+    var icon = $(this).find("i");
+
+    if (!icon.hasClass("spin")) {
+      icon.addClass("spin");
+      setTimeout(function () {
+        icon.removeClass("spin");
+      }, 300);
+    }
+
+    passwordWrite();
+  });
 });
